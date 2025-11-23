9eeb410a6112b0b3f388ee39c4712510495c01a1
mthierman <mthierman@gmail.com>
2025-11-22 17:47:59 -0700

    Cleanup main

--- example/empty_project/main.cpp
+++ example/empty_project/main.cpp
@@ -2,8 +2,7 @@
 import std;
 import my_module;
 
-// https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
-auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
+auto wmain() -> int {
     std::println();
     fmt::print("Hello World!\n");
 

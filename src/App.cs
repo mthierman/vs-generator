fd7a7ebf3f5592b58c4f9c2df316c7266a4e136f
mthierman <mthierman@gmail.com>
2025-11-27 18:14:04 -0700

    Remove Project

--- src/App.cs
+++ src/App.cs
@@ -111,6 +111,55 @@ public static class App
         return process.ExitCode;
     }
 
+    public static async Task<int> New()
+    {
+        if (Directory.EnumerateFileSystemEntries(Environment.CurrentDirectory).Any())
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("Directory is not empty");
+            Console.ResetColor();
+
+            return 1;
+        }
+
+        var cwd = Environment.CurrentDirectory;
+        var vcpkgManifest = Path.Combine(cwd, "vcpkg.json");
+        var vcpkgConfig = Path.Combine(cwd, "vcpkg-configuration.json");
+
+        if (File.Exists(App.Paths.Core.Manifest) || File.Exists(vcpkgManifest) || File.Exists(vcpkgConfig))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("Project already has a manifest file");
+            Console.ResetColor();
+
+            return 1;
+        }
+
+        await File.WriteAllTextAsync(Path.Combine(cwd, App.Paths.Manifest.FileName), "{}");
+
+        await VisualStudio.RunVcpkg("new", "--application");
+
+        if (!Directory.Exists(App.Paths.Core.Src))
+            Directory.CreateDirectory(App.Paths.Core.Src);
+
+        var app_cpp = Path.Combine(App.Paths.Core.Src, "app.cpp");
+
+        if (!File.Exists(app_cpp))
+        {
+            await File.WriteAllTextAsync(app_cpp, @"
+#include <print>
+
+auto wmain() -> int {
+    std::println(""Hello, World!"");
+
+    return 0;
+}
+".Trim());
+        }
+
+        return 0;
+    }
+
     // private static readonly SemaphoreSlim ConsoleLock = new SemaphoreSlim(1, 1);
     private static RootCommand RootCommand { get; } = new RootCommand($"C++ build tool\nversion {Version}");
     private static Argument<VisualStudio.BuildConfiguration> BuildConfiguration = new("BuildConfiguration") { Arity = ArgumentArity.ZeroOrOne, Description = "Build Configuration (debug or release). Default: debug" };
@@ -200,7 +249,7 @@ public static class App
 
         SubCommand["new"].SetAction(async parseResult =>
         {
-            return await Project.New();
+            return await New();
         });
 
         SubCommand["install"].SetAction(async parseResult =>
